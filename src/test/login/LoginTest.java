3e955d59fa5ba0c40a2901bcfe79511075e4c8e3
OleksandrStepaniuk <alexstepalina@gmail.com>
2017-03-14 18:05:24 +0200

    Please read and do adding webdriver to the path in win 7 and 10
    Signed-off-by: OleksandrStepaniuk <alexstepalina@gmail.com>

--- src/test/login/LoginTest.java
+++ src/test/login/LoginTest.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by admin_alex on 3/14/2017.
  */
 public class LoginTest {
+    private WebDriver driver;
+
+    //System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");   ADD WEBDRIVER TO PATH!!!!!!!!
+    //WebDriver driver = new ChromeDriver();
+
 }
